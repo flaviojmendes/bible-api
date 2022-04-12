8600e918ed46e05cdfd9e1410ad8ccf7b7c0becb
Mendes <flaviojmendes@gmail.com>
2022-04-12 01:23:47 +0100

    docker

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         uvicorn.run("main:app",
                     host="0.0.0.0",
                     port=8000,
-                    reload=True,
+                    reload=True,    
                     ssl_keyfile=os.environ["PRIVATE_KEY"],
                     ssl_certfile=os.environ["CERT"]
                     )
