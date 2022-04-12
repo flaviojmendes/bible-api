546aeb1e235750b6da3d7c4312a0108cba87b084
Mendes <flaviojmendes@gmail.com>
2022-04-12 01:46:47 +0100

    docker

--- service/youtube_service.py
+++ service/youtube_service.py
@@ -6,5 +6,5 @@ from config.config import MAX_VIDEO_RESULTS
 class YoutubeService:
 
     def search_video_by_text(self, search_term):
-        videos = VideosSearch(search_term, language="pt", region='BR', limit = MAX_VIDEO_RESULTS)
+        videos = VideosSearch(search_term+" portugues", language="pt", region='BR', limit = MAX_VIDEO_RESULTS)
         return videos
